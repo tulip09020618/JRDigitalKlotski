967ef130f59523b87c33909b04bab34c0ae485ec
wangjinrui <wangjinrui@hongqitech.com>
2018-04-25 18:20:54 +0800

    创建项目，搭建框架

---
+++ JRDigitalKlotski/JRDigitalKlotski/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRDigitalKlotski
+//
+//  Created by hqtech on 2018/4/25.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
